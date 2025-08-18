4df09341cac151cbbc506dcb3a4e866702ebe9d4
Anouarbery <sirinaberriri@gmail.com>
2025-08-18 12:50:34 +0100

    Update twrp_a56x.mk

--- twrp_a56x.mk
+++ twrp_a56x.mk
@@ -6,13 +6,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit a56x device.mk
-$(call inherit-product, device/samsung/a55x/device.mk)
+$(call inherit-product, device/samsung/a56x/device.mk)
 
 # Product Name
 PRODUCT_RELEASE_NAME := a56x
 PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
 PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-A566R
+PRODUCT_MODEL := SM-A566B
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
